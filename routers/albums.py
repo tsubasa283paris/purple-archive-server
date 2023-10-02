cdcf7bc93573ad1b7a0d888f4c4d1397183d6a71
tsubasa283paris <tsubasa283paris@gmail.com>
2023-10-02 22:43:47 +0900

    1: Implement user authorization with JWT

--- routers/albums.py
+++ routers/albums.py
@@ -3,6 +3,7 @@ from typing import List
 from fastapi import Depends, APIRouter
 from sqlalchemy.orm import Session
 
+from auth.auth import UserInfo
 from sql_interface import crud, schemas
 from sql_interface.database import get_db
 
@@ -11,6 +12,11 @@ router = APIRouter()
 
 
 @router.get("/albums/", response_model=List[schemas.AlbumRead])
-def read_items(offset: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+def read_items(
+    user_info: UserInfo,
+    offset: int = 0,
+    limit: int = 100,
+    db: Session = Depends(get_db)
+):
     items = crud.get_albums(db, offset=offset, limit=limit)
     return items
--- routers/users.py
+++ routers/users.py
@@ -3,6 +3,7 @@ from typing import List
 from fastapi import Depends, APIRouter, HTTPException
 from sqlalchemy.orm import Session
 
+from auth.auth import UserInfo
 from sql_interface import crud, schemas
 from sql_interface.database import get_db
 
@@ -11,13 +12,28 @@ router = APIRouter()
 
 
 @router.get("/users/", response_model=List[schemas.UserRead])
-def read_users(offset: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+def read_users(
+    user_info: UserInfo,
+    offset: int = 0, limit: int = 100,
+    db: Session = Depends(get_db),
+):
     users = crud.get_users(db, offset=offset, limit=limit)
     return users
 
 
+@router.get("/users/me", response_model=schemas.UserReadDeep)
+def read_user_me(
+    user_info: UserInfo,
+):
+    return user_info
+
+
 @router.get("/users/{user_id}", response_model=schemas.UserReadDeep)
-def read_user(user_id: int, db: Session = Depends(get_db)):
+def read_user(
+    user_info: UserInfo,
+    user_id: int,
+    db: Session = Depends(get_db)
+):
     db_user = crud.get_user(db, user_id=user_id)
     if db_user is None:
         raise HTTPException(status_code=404, detail="Specified user does not exist.")
@@ -25,7 +41,11 @@ def read_user(user_id: int, db: Session = Depends(get_db)):
 
 
 @router.post("/users/", response_model=schemas.UserRead)
-def create_user(user: schemas.UserWrite, db: Session = Depends(get_db)):
+def create_user(
+    user_info: UserInfo,
+    user: schemas.UserWrite,
+    db: Session = Depends(get_db)
+):
     db_user = crud.get_user(db, id=user.id)
     if db_user:
         raise HTTPException(status_code=400, detail="User ID already registered.")
