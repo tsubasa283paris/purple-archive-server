bb791952dd0fb4df5d40225616b3e3051b724586
tsubasa283paris <tsubasa283paris@gmail.com>
2023-10-07 21:59:04 +0900

    8: Add functionality to periodically remove temp_albums

--- main.py
+++ main.py
@@ -5,21 +5,22 @@ from fastapi.responses import JSONResponse
 from auth.auth import auth_router
 from dotenv import load_dotenv
 from routers import users, albums, bookmarks, tags, gamemodes
+from routers.temp_albums_cleaner import TempAlbumsCleaner
 
 
 # load .env
 load_dotenv()
 
-
-# app = FastAPI(dependencies=[Depends(get_query_token)])
 app = FastAPI()
 
-
+# exception handler to dump what's wrong
 @app.exception_handler(RequestValidationError)
 async def handler(request: Request, exc: RequestValidationError):
     print(exc)
-    return JSONResponse(content={}, status_code=status.HTTP_422_UNPROCESSABLE_ENTITY)
-
+    return JSONResponse(
+        content={},
+        status_code=status.HTTP_422_UNPROCESSABLE_ENTITY
+    )
 
 app.include_router(auth_router)
 app.include_router(users.router)
@@ -27,3 +28,11 @@ app.include_router(albums.router)
 app.include_router(bookmarks.router)
 app.include_router(tags.router)
 app.include_router(gamemodes.router)
+
+# kick temp_albums cleaner
+temp_albums_cleaner = TempAlbumsCleaner()
+
+@app.on_event("shutdown")
+async def shutdown_event():
+    global temp_albums_cleaner
+    temp_albums_cleaner.stop()
